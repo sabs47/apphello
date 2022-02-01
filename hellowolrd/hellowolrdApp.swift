04b87ea0104a1266062745de94514417b39eaa65
El hadji.M.sabaly <hadjisabs@gmail.com>
2022-02-01 22:32:19 +0100

    Add files via upload

---
+++ hellowolrd/hellowolrdApp.swift
@@ -0,0 +1,17 @@
+//
+//  hellowolrdApp.swift
+//  hellowolrd
+//
+//  Created by SABS on 01/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct hellowolrdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
